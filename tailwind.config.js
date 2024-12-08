953d5b6dba0a366cd2be68fb7ee938b7adefbf16
Aya Osama <ayaramadan2011@gmail.com>
2024-12-08 05:11:11 +0200

    add nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        graii: 'rgba(84, 84, 84, 1)',
+        navbtn:'rgba(31, 31, 31, 1)'
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
