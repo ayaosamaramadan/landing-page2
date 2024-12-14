30203529cd7e7b97c581d7c891a1706ce49f6622
Aya Osama <ayaramadan2011@gmail.com>
2024-12-14 17:38:36 +0200

    handle menu

---
+++ index.js
@@ -0,0 +1,11 @@
+let menu = document.getElementById('menu');
+
+function handleMenu(){
+  if (menu.style.opacity === '0' ) {
+ menu.classList.add('nav-active');
+ menu.style.opacity = '1';
+} else {
+ menu.classList.remove('nav-active');
+ menu.style.opacity = '0';
+}
+}
